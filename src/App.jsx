cd34bb01656fbecb8be2bf381fa00d167766eef4
Maksim Morozov <maxmorozov.art@gmail.com>
2023-05-20 23:33:37 +0300

    Add MainPage and About components
    Created MainPage and About components, added them to App as Routes.
    No actual content on these pages for now, just for testing routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import MainPage from './components/MainPage';
+import About from './components/About';
+import NavBarComponent from './components/NavBar';
+
 function App() {
-  return <div>Test</div>;
+  return (
+    <Router>
+      <NavBarComponent />
+
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
